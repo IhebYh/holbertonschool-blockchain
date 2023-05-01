36b27b980113b3035267ae88734a702aa8136f54
Iheb Yahyaoui <iheb.yah@gmail.com>
2023-05-01 18:15:37 +0100

    blockchain difficulty

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -122,5 +122,8 @@ int block_is_valid(block_t const *block, block_t const *prev_block);
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 							uint32_t difficulty);
 void block_mine(block_t *block);
+int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
+	uint32_t difficulty);
+uint32_t get_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH]);
 
 #endif /* _BLOCKCHAIN_H_ */
