8189121bb236c8c5cbb58cbc1826cc9f4e2936f5
Iheb Yahyaoui <iheb.yah@gmail.com>
2023-05-01 18:32:34 +0100

    add task 3

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -122,8 +122,6 @@ int block_is_valid(block_t const *block, block_t const *prev_block);
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 							uint32_t difficulty);
 void block_mine(block_t *block);
-int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
-	uint32_t difficulty);
-uint32_t get_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH]);
+uint32_t blockchain_difficulty(blockchain_t const *blockchain);
 
 #endif /* _BLOCKCHAIN_H_ */
--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -1,38 +1,31 @@
 #include "blockchain.h"
 
 /**
- * get_difficulty - computers number of leading bits in hash
- * @hash: hash buffer
- * Return: computed difficulty
+ * blockchain_difficulty - adjust difficulty for next block of blockchain
+ * @blockchain: pointer to working blockchain
+ * Return: adjusted difficulty
  */
-uint32_t get_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH])
+uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 {
-	uint8_t *ptr = (uint8_t *)hash;
-	uint32_t difficulty = 0;
-	int i;
+	block_t *last_block, *adjusted_block;
+	uint64_t expected_time, actual_time;
 
-	for (; ptr < hash + SHA256_DIGEST_LENGTH; ptr++)
-	{
-		for (i = 7; i >= 0; i--)
-		{
-			if ((*ptr >> i) & 1)
-				return (difficulty);
-			difficulty++;
-		}
-	}
-	return (difficulty);
-}
-
-/**
- * hash_matches_difficulty - determines if hash matches difficulty
- * @hash: hash buffer
- * @difficulty: difficulty to match
- * Return: 1 if matches else 0
- */
-int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
-	uint32_t difficulty)
-{
-	if (!hash)
+	if (!blockchain)
 		return (0);
-	return (get_difficulty(hash) >= difficulty);
+	last_block = llist_get_tail(blockchain->chain);
+	if (!last_block)
+		return (0);
+	if (last_block->info.index == 0 ||
+		last_block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL)
+		return (last_block->info.difficulty);
+	adjusted_block = llist_get_node_at(blockchain->chain,
+		last_block->info.index + 1 - DIFFICULTY_ADJUSTMENT_INTERVAL);
+	expected_time = DIFFICULTY_ADJUSTMENT_INTERVAL * BLOCK_GENERATION_INTERVAL;
+	actual_time = last_block->info.timestamp - adjusted_block->info.timestamp;
+	if (actual_time * 2 < expected_time)
+		return (last_block->info.difficulty + 1);
+	else if (actual_time > 2 * expected_time)
+		return (last_block->info.difficulty > 0 ?
+			last_block->info.difficulty - 1 : 0);
+	return (last_block->info.difficulty);
 }
