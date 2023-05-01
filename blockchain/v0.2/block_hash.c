3913aa24ade1b25b2021741b6f54d60ab42dc3e1
Iheb Yahyaoui <iheb.yah@gmail.com>
2023-05-01 17:57:08 +0100

    init v0.2+ task #0

---
+++ blockchain/v0.2/block_hash.c
@@ -0,0 +1,19 @@
+#include "blockchain.h"
+
+/**
+ * block_hash - computes hash of block
+ * @block: pointer to block to hash
+ * @hash_buf: buffer to store hash/digest
+ * Return: pointer to buffer
+ */
+uint8_t *block_hash(block_t const *block,
+					uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+	size_t len;
+
+	if (!block || !hash_buf)
+		return (NULL);
+
+	len = sizeof(block->info) + block->data.len;
+	return (sha256((int8_t const *)block, len, hash_buf));
+}
