75b7abf9343309e5c1c94c219fa73a45f7e2b6f4
Iheb Yahyaoui <iheb.yah@gmail.com>
2023-05-04 21:18:42 +0100

    block hash

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -9,12 +9,8 @@
 uint8_t *block_hash(block_t const *block,
 					uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t len;
+	size_t len = sizeof(block->info) + block->data.len;
 
-	if (!block || !hash_buf)
-		return (NULL);
-
-	len = sizeof(block->info) + block->data.len;
 	sha256((int8_t const *)&(block->info), len, hash_buf);
 	return (hash_buf);
 }
